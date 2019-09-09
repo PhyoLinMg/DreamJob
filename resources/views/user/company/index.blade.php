d47657b846dc64143b79611f4f74d00045ea2fa6
PhyoLinMg <gangsterplm@gmail.com>
2019-09-09 12:03:52 +0630

    about us

--- resources/views/user/company/index.blade.php
+++ resources/views/user/company/index.blade.php
@@ -11,9 +11,11 @@
 					<h5 class="card-title">{{$company->name}}</h5>
 					<h6 class="card-subtitle mb-2 text-muted">{{$company->address}}</h6>
 					<p class="card-text">{{$company->description}}</p>
-					@for ($i = 0; $i <2 ; $i++)
-						<a href="{{ route('get',['id'=>$company->jobs[$i]->id]) }}" class="card-link">{{$company->jobs[$i]->post}}</a>
-					@endfor
+						@foreach ($company->jobs as $job)
+							<a href="{{ route('get',['id'=>$job->id]) }}" class="card-link">{{$job->post}}</a>
+	
+						@endforeach
+											
 					<br>
 					<a href="/job" class="text-center">More....</a>
 					
